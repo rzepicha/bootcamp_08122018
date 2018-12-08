c0410149436a1fb58626394dd6844600bde708b6
rzepicha <rzepicha_g@autograf.pl>
2018-12-08 11:29:20 +0100

    init

---
+++ pierwszy.py
@@ -0,0 +1,22 @@
+# policz pole trójkąta o podst 10 i wys 5
+podst=10
+wys=5
+pt=(1/2)*podst*wys
+print ("Pole trójkąta wynosi: ", pt)
+
+#policz pole koła o promieniu 7
+r=7
+pk=3.14*(r**2)
+print ("Pole koła wynosi: ", pk)
+
+# policz pole trapezu o dł podstaw 3 i 9 i wys 5
+p1=3
+p2=9
+h=5
+ptrap=((p1+p2)*h)/2
+print ("Pole trapezu wynosi: ",ptrap)
+
+# policz objętość kuli o zadanym promieniu
+rkuli=7
+obj=(4/3)*3.14*(rkuli**3)
+print ("Objętość koła wynosi: ", obj)
